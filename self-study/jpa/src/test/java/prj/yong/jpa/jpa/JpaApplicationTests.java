f0afda00b10a556f795e4c4bf3312bf062435b77
justdoanything <pure_yong@naver.com>
2022-03-28 13:50:56 +0900

    JPA 연습 프로젝트 추가

---
+++ self-study/jpa/src/test/java/prj/yong/jpa/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.yong.jpa.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
