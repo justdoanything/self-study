c240bf565a62d5ef2815ee12d77152f58c694b50
justdoanything <pure_yong@naver.com>
2022-04-09 11:45:46 +0900

    이펙티브자바 내용 추가

--- self-study/JAVA/src/CleanerSample.java
+++ self-study/JAVA/src/CleanerSample.java
@@ -1,5 +1,11 @@
 import java.lang.ref.Cleaner;
 
+
+/**
+ * State 인스턴스가 Room 인스턴스를 참조하면 순환참조가 일어나서 청소되지 않는다.
+ * try-resources문을 사용해서 Room 객체를 사용하면 프로그램이 정상적으로 청소됨("====방 청소!")을 볼 수 있지만
+ * 단순하게 new Room(10);으로 선언해서 사용한다면 언제 청소될지 알 수 없다.
+ */
 public class CleanerSample implements AutoCloseable {
     private static final Cleaner cleaner = Cleaner.create();
 
---
+++ self-study/JAVA/src/ImplementEquals.java
@@ -0,0 +1,26 @@
+public class ImplementEquals {
+    private final short areaCode, prefix, lineNum;
+    
+    public ImplementEquals(int areaCode, int prefix, int lineNum){
+        this.areaCode = rangeCheck(areaCode, 999, "locale code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 9999, "number");
+    }
+
+    private static short rangeCheck(int val, int max, String arg){
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }   
+
+    @Override
+    public boolean equals(Object o){
+        if(o == this)
+            return true;
+        if(!(o instanceof ImplementEquals))
+            return false;
+        ImplementEquals ie = (ImplementEquals) o;
+        return ie.lineNum == lineNum && ie.prefix == prefix && ie.areaCode == areaCode;
+    }
+
+}
