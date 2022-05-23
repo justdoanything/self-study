f66c3bab7a4b940db928a2edd6b1021aa15e2e41
yongwoo <pure_yong@naver.com>
2022-05-23 20:36:54 +0900

    TCT 실습 추가

--- self-study/TCT/DCX/script.js
+++ self-study/TCT/DCX/script.js
@@ -18,8 +18,8 @@ function clickLocation(lang) {
   if (lang == "kor") {
     document.getElementById("kor").style.color = "red";
     document.getElementById("eng").style.color = "black";
-    document.getElementById("chartList").innerHTML = "";
 
+    document.getElementById("chartList").innerHTML = "";
     fetch("http://localhost:3300/v1/chart/domestic")
       .then((response) => response.json())
       .then((data) => {
@@ -53,9 +53,36 @@ function clickLocation(lang) {
     document.getElementById("kor").style.color = "black";
     document.getElementById("eng").style.color = "red";
 
+    document.getElementById("chartList").innerHTML = "";
     fetch("http://localhost:3300/v1/chart/overseas")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        var td = "<table/>";
+        for (var index in data.chartList) {
+          var id = data.chartList[index].id;
+          var imageUrl = "images/" + data.chartList[index].imageUrl;
+          var rank = data.chartList[index].rank;
+          var singer = data.chartList[index].singer;
+          var title = data.chartList[index].title;
+
+          td +=
+            "<tr>" +
+            "<td>" +
+            rank +
+            "</td>" +
+            '<td><img src="' +
+            imageUrl +
+            '" /></td>' +
+            "<td>" +
+            title +
+            "</td>" +
+            '<td style="text-align: right">' +
+            singer +
+            "</td>" +
+            "</tr>";
+        }
+        document.getElementById("chartList").innerHTML = td;
+      });
   } else {
   }
 }
