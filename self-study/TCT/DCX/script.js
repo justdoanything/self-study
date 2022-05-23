7f4dd86abc9ff77b431c122c09a780d2ce19475d
yongwoo <pure_yong@naver.com>
2022-05-23 20:35:52 +0900

    TCT 실습 추가

--- self-study/TCT/DCX/script.js
+++ self-study/TCT/DCX/script.js
@@ -18,10 +18,37 @@ function clickLocation(lang) {
   if (lang == "kor") {
     document.getElementById("kor").style.color = "red";
     document.getElementById("eng").style.color = "black";
+    document.getElementById("chartList").innerHTML = "";
 
     fetch("http://localhost:3300/v1/chart/domestic")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        var td = "<table/>";
+        for (var index in data.chartList) {
+          var id = data.chartList[index].id;
+          var imageUrl = "images/" + data.chartList[index].imageUrl;
+          var rank = data.chartList[index].rank;
+          var singer = data.chartList[index].singer;
+          var title = data.chartList[index].title;
+
+          td +=
+            "<tr>" +
+            "<td>" +
+            rank +
+            "</td>" +
+            '<td><img src="' +
+            imageUrl +
+            '" /></td>' +
+            "<td>" +
+            title +
+            "</td>" +
+            '<td style="text-align: right">' +
+            singer +
+            "</td>" +
+            "</tr>";
+        }
+        document.getElementById("chartList").innerHTML = td;
+      });
   } else if (lang == "eng") {
     document.getElementById("kor").style.color = "black";
     document.getElementById("eng").style.color = "red";
