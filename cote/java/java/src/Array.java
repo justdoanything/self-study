ee99c0faf9e361bc6e63f5b7e62575e6ed417e2a
justdoanything <pure_yong@naver.com>
2022-05-31 14:26:34 +0900

    JAVA 추가

--- cote/java/java/src/Array.java
+++ cote/java/java/src/Array.java
@@ -175,7 +175,7 @@ public class Array {
         printArray(array);
     }
     
-    private static void printArray(int[][] array) {
+    public static void printArray(int[][] array) {
         for(int i=0; i<array.length; i++){
             for(int j=0; j<array[0].length; j++){
                 System.out.print(array[i][j] + "\t");
---
+++ cote/java/java/src/ArrayRotation.java
@@ -0,0 +1,70 @@
+public class ArrayRotation {
+    
+    // 시계방향 - 상하좌우
+    int[] dx4 = {-1, 1, 0, 0};
+    int[] dy4 = {0, 0, -1, 1};
+
+    // 시계방향 - 상하좌우대각선
+    int[] dx8 = { -1, -1, 0, 1, 1, 1, 0, -1};
+    int[] dy8 = { 0, 1, 1, 1, 0, -1, -1, -1};
+
+    // 문자열을 index로 변환
+    String posEng = "d4";
+    int posEngX = posEng.charAt(0)-'a';
+    int posEngY = posEng.charAt(1)-'0';
+    
+    public static void main(String[] args){
+        int N = 7;
+        int[][]  map = new int[N][N];
+        int posX = 3;
+        int posY = 3;
+        int num = 1;
+
+        map[posX][posY] = 1;
+        System.out.println("================ START");
+        Array.printArray(map);
+
+        move4Ways(map, posX, posY, num, N);
+
+        move8Ways(map, posX, posY, num, N);
+        
+    }
+
+    private static void move4Ways(int[][] map, int posX, int posY, int num, int N){
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        System.out.println("================ 상하좌우");
+        for(int d=1; d<=num; d++){
+            for(int i=0; i<4; i++){
+                int nextX = posX + dx[i]*d;
+                int nextY = posY + dy[i]*d;
+
+                if(nextX >= 0 && nextX < N 
+                    && nextY >= 0 && nextY < N){
+                    map[nextX][nextY] = 1;
+                }
+            }
+        }
+        Array.printArray(map);
+    }
+
+    private static void move8Ways(int[][] map, int posX, int posY, int num, int N){
+        int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1};
+        int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1};
+
+        System.out.println("================ 상하좌우대각선");
+        for(int d=1; d<=num; d++){
+            for(int i=0; i<8; i++){
+                int nextX = posX + dx[i]*d;
+                int nextY = posY + dy[i]*d;
+
+                if(nextX >= 0 && nextX < N 
+                    && nextY >= 0 && nextY < N){
+                    map[nextX][nextY] = 1;
+                }
+            }
+        }
+        Array.printArray(map);
+    }
+}
