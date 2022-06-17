d62586dc230ed1fb99332eb7131a73e6df6092b3
justdoanything <pure_yong@naver.com>
2022-06-17 13:52:50 +0900

    내용 추가

--- self-study/EffectiveJava/src/test/LambdaTest.java
+++ self-study/EffectiveJava/src/test/LambdaTest.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class LambdaTest {
@@ -16,8 +18,31 @@ public class LambdaTest {
         inventory.add(new Apple("8", 40));
 
         inventory.stream()
-            .filter((Apple a) -> a.getWeight() > 400)
+            .filter((Apple a) -> a.getWeight() > 100)
             .collect(Collectors.toList())
             .forEach(System.out::println);
+        
+        inventory.sort(Comparator.comparing(Apple::getWeight)
+        .reversed()
+        .thenComparing(Apple::getName));
+
+        Predicate<Apple> predicateHeavyApple = (Apple a) -> a.getWeight() > 150;
+        List<Apple> result = filterAppleWeight(inventory, predicateHeavyApple
+                                                            .and((Apple a) -> a.getName().equals("3")));
+        System.out.println("=======================");
+        result.forEach(System.out::println);
+        
+
     }
+
+    static List<Apple> filterAppleWeight(List<Apple> list, Predicate<Apple> p){
+        List<Apple> result = new ArrayList<>();
+        for(Apple apple : list){
+            if(p.test(apple))
+                result.add(apple);
+        }
+        return result;
+    };
+
+    
 }
