c0f9f9b171011759bc09ee87c2b9c6c78d41346d
justdoanything <pure_yong@naver.com>
2022-06-21 20:28:57 +0900

    내용추가

--- self-study/JAVA/src/modern/ModernJavaPractice.java
+++ self-study/JAVA/src/modern/ModernJavaPractice.java
@@ -1,8 +1,7 @@
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ModernJavaPractice {
@@ -10,31 +9,41 @@ public class ModernJavaPractice {
     public static void main(String[] args) {
         List<Apple> inventory = new ArrayList<>();
         inventory.add(new Apple("1", 100));
-        inventory.add(new Apple("2", 10));
+        inventory.add(new Apple("2", 100));
         inventory.add(new Apple("3", 200));
         inventory.add(new Apple("4", 20));
         inventory.add(new Apple("5", 300));
-        inventory.add(new Apple("6", 30));
+        inventory.add(new Apple("6", 300));
         inventory.add(new Apple("7", 400));
         inventory.add(new Apple("8", 40));
 
-        inventory.stream()
-            .filter((Apple a) -> a.getWeight() > 100)
-            .collect(Collectors.toList())
-            .forEach(System.out::println);
+        // inventory.stream()
+        //     .filter((Apple a) -> a.getWeight() > 100)
+        //     .collect(Collectors.toList())
+        //     .forEach(System.out::println);
         
-        inventory.sort(Comparator.comparing(Apple::getWeight)
-        .reversed()
-        .thenComparing(Apple::getName));
+        // inventory.sort(Comparator.comparing(Apple::getWeight)
+        // .reversed()
+        // .thenComparing(Apple::getName));
 
-        Predicate<Apple> predicateHeavyApple = (Apple a) -> a.getWeight() > 150;
-        List<Apple> result = filterAppleWeight(inventory, predicateHeavyApple
-                                                            .and((Apple a) -> a.getName().equals("3")));
-        System.out.println("=======================");
-        result.forEach(System.out::println);
+        // Predicate<Apple> predicateHeavyApple = (Apple a) -> a.getWeight() > 150;
+        // List<Apple> result = filterAppleWeight(inventory, predicateHeavyApple
+        //                                                     .and((Apple a) -> a.getName().equals("3")));
+        // System.out.println("=======================");
+        // result.forEach(System.out::println);
 
-        AppleConstructor<String, Integer, Integer, Apple> apple = Apple::new;
-        apple.apply("tomato", 10, 100);
+        // AppleConstructor<String, Integer, Integer, Apple> apple = Apple::new;
+        // apple.apply("tomato", 10, 100);
+
+        List<String> temp = inventory.parallelStream()
+                    .filter(apple -> apple.getWeight() > 100)
+                    .sorted(Comparator.comparing(Apple::getWeight))
+                    .map(Apple::getName)
+                    .collect(Collectors.toList());
+        temp.forEach(System.out::println);
+
+        Map<Integer, List<Apple>> groupApple = inventory.stream().collect(Collectors.groupingBy(Apple::getWeight));
+        System.out.println(groupApple);
     }
 
     static List<Apple> filterAppleWeight(List<Apple> list, Predicate<Apple> p){
