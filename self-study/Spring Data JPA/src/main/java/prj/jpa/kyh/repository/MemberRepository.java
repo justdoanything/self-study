71c16f5d8af203f687437b80d3fb6361d725d751
justdoanything <pure_yong@naver.com>
2022-07-09 17:43:42 +0900

    내용 추가

--- self-study/Spring Data JPA/src/main/java/prj/jpa/kyh/repository/MemberRepository.java
+++ self-study/Spring Data JPA/src/main/java/prj/jpa/kyh/repository/MemberRepository.java
@@ -4,12 +4,17 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import prj.jpa.kyh.dto.MemberDto;
@@ -68,5 +73,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @EntityGraph(attributePaths = {"team"})
     List<Member> findAll();
 
+    /*
+     * Hint & Lock
+     */
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByName(String name);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByName(String name);
 }
