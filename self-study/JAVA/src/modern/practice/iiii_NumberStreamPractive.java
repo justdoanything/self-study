5084c00343b1d191bea5af4a4a04a5e4faf65526
justdoanything <pure_yong@naver.com>
2022-07-17 19:50:47 +0900

    내용 추가

--- self-study/JAVA/src/modern/practice/iiii_NumberStreamPractive.java
+++ self-study/JAVA/src/modern/practice/iiii_NumberStreamPractive.java
@@ -2,6 +2,10 @@ package src.modern.practice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import src.modern.Dish;
 
@@ -33,5 +37,64 @@ public class iiii_NumberStreamPractive {
                 .average()
                 .ifPresent(System.out::println);
         
-    }
+        // OptionalInt
+        OptionalInt oi = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .map(c -> c * -1)
+                .max();
+        int max = oi.orElse(1);
+
+        /*
+         * 숫자 범위 지정(range, rangeClosed)
+         */ 
+        IntStream.range(0, 10) // 0 < x < 100
+                    .filter(n -> n % 2 == 0)
+                    .forEach(System.out::println);
+        
+        IntStream.rangeClosed(0, 10) // 0 <= x <= 100
+                    .filter(n -> n % 2 == 0)
+                    .forEach(System.out::println);
+
+        int[] array = {1, 2, 3, 4};
+        List<int[]> list = Arrays.asList(array);
+
+        List<Integer> list2 = Arrays.stream(array)
+                                        .boxed()
+                                        .collect(Collectors.toList());
+
+        
+        /*
+         * 피타고라스
+         */ 
+        Stream<int[]> pythagorean = IntStream.rangeClosed(1, 100)
+                                                .boxed()
+                                                .flatMap(a ->
+                                                    IntStream.rangeClosed(a, 100)
+                                                        .filter(b -> Math.sqrt(a*a + b*b) % 1 == 0)
+                                                        .mapToObj(b ->
+                                                            new int[]{a, b, (int)Math.sqrt(a*a + b*b)}));
+
+        Stream<double[]> pythagorean2 = IntStream.rangeClosed(1, 100)
+                                                    .boxed()
+                                                    .flatMap(a -> IntStream.rangeClosed(a, 100)
+                                                    .mapToObj(b -> new double[]{a, b, Math.sqrt((a*a + b*b))})
+                                                    .filter(t -> t[2] % 1 ==0 ));
+        pythagorean.limit(5)
+                    .forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+        pythagorean2.limit(5)
+                    .forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        
+        /*
+         * 값으로 Stream 생성
+         */
+        Stream<String> strStream = Stream.of("Hello", "World", "In", "Java");
+        strStream.map(String::toUpperCase).forEach(System.out::println);
+        strStream = Stream.empty();
+        strStream.forEach(System.out::println);
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.stream(numbers).sum());
+        
+    }   
 }
