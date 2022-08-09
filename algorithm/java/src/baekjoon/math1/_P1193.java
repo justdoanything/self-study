ef7d36cd93415eec4a6518c401a48f429b7d7c9e
justdoanything <pure_yong@naver.com>
2022-08-09 17:58:18 +0900

    알고리즘 연습

--- algorithm/java/src/baekjoon/math1/_P1193.java
+++ algorithm/java/src/baekjoon/math1/_P1193.java
@@ -1,31 +1,32 @@
 package src.baekjoon.math1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class _P1193 {
     
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        scanner.nextLine();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
 
-        int index = 1;
-        // 행과열이다. i/j 를 출력하면된다.
-        index = 1;
-        // 전체 탐색을 위한 변수 c , 범위는 row+col
-        for(int c=0; c<=100; c++){
-            for(int i=0; i<100; i++){
-                for(int j=0; j<100; j++){
-                    // i와 j의 합이 c와 같은 곳에서만 출력
-                    if(i+j == c){
-                        if(index == input){
-                            System.out.println((j+1) + "/" + (i+1));
-                            return;
-                        }
-                        index++;
-                    }
-                }
+        int fibo = 1;
+        int count = 1;
+
+        while(true){
+            if(fibo > input){
+                break;
+            }else {
+                fibo += count;
+                count++;
             }
+        }
+
+        int row = count-1;
+        int rest = input-(fibo-(count-1));
+        if(row % 2 == 0){
+            System.out.println((1+rest) + "/" + (row-rest));
+        }else {
+            System.out.println((row-rest) + "/" + (1+rest));
         }
     }
 }
