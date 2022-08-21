e20c05b07a5b06cfd77e9b5da4bf97eb0ac9750d
justdoanything <pure_yong@naver.com>
2022-08-21 17:50:03 +0900

    내용 수정

--- algorithm/java/src/programmers/P04_debug.java
+++ algorithm/java/src/programmers/P04_debug.java
@@ -1,19 +1,5 @@
 package programmers;
 
-/*
-BFS 를 이용해서 start 번 정점에서 5 번 정점으로 가기 위한 최소 이동 횟수를 구하려 합니다. 
-solution 함수는 아래 그래프의 "start" 번 정점에서 5번 정점까지 가는 최소 이동 횟수를 반환해야 합니다.
-
-초기 코드는 원하는 결과가 나오지 않는 버그가 나오는 코드입니다. 
-초기 코드를 정확히 한 줄을 수정해서 최소 이동 횟수를 리턴할 수 있는 코드를 완성 시켜주세요. 
-
-테스트 1
-입력값 〉	1
-기댓값 〉	2
-실행 결과 〉	실행한 결괏값 7이 기댓값 2과 다릅니다.
-
-*/
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
