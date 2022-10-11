dfd40b548315ec0d124dc323c5dcaa6799d3abc2
justdoanything <pure_yong@naver.com>
2022-10-12 08:22:30 +0900

    내용 추가

--- WIS/Java/src/main/java/modern/model/common/CommonVO.java
+++ WIS/Java/src/main/java/modern/model/common/CommonVO.java
@@ -1,6 +1,20 @@
 package modern.model.common;
 
-public class CommonVO {
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommonVO {
+    @Email
+    @NotNull
+    @ApiModelProperty(value="", notes="", example = "")
+    private String attribute1;
+    private Object attribute2;
 
 }
