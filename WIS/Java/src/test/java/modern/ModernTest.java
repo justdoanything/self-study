dec0e790ead723dafd6068a174c69129cc234a0d
justdoanything <pure_yong@naver.com>
2022-10-21 17:40:49 +0900

    내용 추가

--- WIS/Java/src/test/java/modern/ModernTest.java
+++ WIS/Java/src/test/java/modern/ModernTest.java
@@ -1,11 +1,18 @@
 package modern;
 
+import modern.controller.SpringController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ModernTest {
 
+    @Autowired
+    SpringController springController;
+
     @Test
-    public void modernTest() {}
+    public void modernTest() {
+        springController.healthCheck(1);
+    }
 }
