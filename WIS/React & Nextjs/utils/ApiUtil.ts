b3bfa5c4f693610c77adad10ed88b37219f3e0e9
justdoanything <pure_yong@naver.com>
2023-03-16 13:07:57 +0900

    axios 추가

--- WIS/React & Nextjs/utils/ApiUtil.ts
+++ WIS/React & Nextjs/utils/ApiUtil.ts
@@ -1,34 +1,105 @@
-import axios, {AxiosInstance} from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import {Endpoint, EndpointPort} from "../models/api/ApiModel";
 
-const getAxiosInstance = (endPointName: string, isLoading: boolean, param?: any): AxiosInstance => {
-    if(isLoading){
-        // show Loading Component
-    }
+export enum Method {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    PATCH = "PATCH"
+}
 
-    setHAxiosHeader();
+export interface QueryParam {
+    [key: string]: string | number | boolean;
+}
 
-    let url = process.env.API_BASE_URL;
-    switch(endPointName) {
-        case Endpoint.BACKEND:
-            url += ':' + EndpointPort.BACKEND.toString();
+export interface BodyParam {
+    queryParam?: QueryParam;
+    bodyParam?: object;
+}
+
+export interface ApiRequest {
+    url: string;
+    method: Method;
+    param?: BodyParam;
+}
+
+export interface ApiResponse<T = any> {
+    statusCode: string;
+    data?: T;
+}
+
+const getAxiosInstance = (param: any): AxiosInstance => {
+
+    /* Method 별 공통 Header 설정 */
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+
+    const axiosInstance = axios.create({
+        params: param || {},
+        headers: { "Cache-Controle": "no-cache" }
+    });
+
+    axiosInstance.interceptors.request.use(
+        (axiosConfig: AxiosRequestConfig): AxiosRequestConfig => {
+            if(axiosConfig?.headers){
+                /* request 공통 Interceptor 설정 */
+                axiosConfig.headers["key"] = "value";
+            }
+            return axiosConfig;
+        },
+        (error: any): Promise<any> => {
+            return Promise.reject(error);
+        }
+    );
+
+    axiosInstance.interceptors.response.use(
+        async (response: any): Promise<any> => {
+            /* response 공통 Interceptor 설정 */
+            return response.data;
+        },
+        async (error: any): Promise<any> => {
+            return error;
+        }
+    );
+
+    return axiosInstance;
+}
+
+const getQueryString = (query?: QueryParam): string => {
+    if(!query) return "";
+    const key = Object.keys(query);
+    const queryString = key.filter((key) => query[key] !== null && query[key] !== undefined)
+        .map((key) => `${key}=${encodeURIComponent(query[key] as string)}`)
+        .join("&");
+    return queryString ? `?${queryString}` : "";
+}
+
+export const callApi = async (apiRequest: ApiRequest): Promise<ApiResponse> => {
+    const url: string = apiRequest.url + getQueryString(apiRequest.param?.queryParam);
+
+    let response: ApiResponse = {
+        statusCode: "ERROR",
+        data: {}
+    };
+
+    switch (apiRequest.method) {
+        case Method.GET :
+            response = await getAxiosInstance(apiRequest.param).get(url);
+            break;
+        case Method.POST:
+            response = await getAxiosInstance(apiRequest.param).post(url, apiRequest.param?.bodyParam);
+            break;
+        case Method.PUT:
+            response = await getAxiosInstance(apiRequest.param).put(url, apiRequest.param?.bodyParam);
+            break;
+        case Method.DELETE:
+            response = await getAxiosInstance(apiRequest.param).delete(url);
+            break;
+        case Method.PATCH:
+            response = await getAxiosInstance(apiRequest.param).patch(url, apiRequest.param?.bodyParam);
             break;
         default:
             break;
     }
-
-    const instance = axios.create({
-        baseURL: url,
-        params: param || {},
-    });
-
-    instance.interceptors.request.use(
-
-    )
-
-    return instance;
-}
-
-const setHAxiosHeader = () => {
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    return response;
 }
