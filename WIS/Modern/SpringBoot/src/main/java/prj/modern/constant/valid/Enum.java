6d327cdde8e37a77ff7cc569904f5fe8b2642e25
justdoanything <pure_yong@naver.com>
2023-03-26 12:01:09 +0900

    내용 추가

--- WIS/Modern/SpringBoot/src/main/java/prj/modern/constant/valid/Enum.java
+++ WIS/Modern/SpringBoot/src/main/java/prj/modern/constant/valid/Enum.java
@@ -7,6 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @Description
+ *  이 클래스에선 Validation에 사용할 annotation 이름, 사용 범위, 사용 시점을 정의하고 valid 로직을 담고 있는 class를 연결한다.
+ *  특정 valid 로직은 ConstraintValidator를 상속받은 클래스에서 정의한다.
+ */
 @Target({ElementType.TYPE_USE, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EnumValidator.class)
--- WIS/Modern/SpringBoot/src/main/java/prj/modern/constant/valid/EnumValidator.java
+++ WIS/Modern/SpringBoot/src/main/java/prj/modern/constant/valid/EnumValidator.java
@@ -8,6 +8,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * @Description
+ *  Java에서 기본적으로 제공하는 원시 타입의 필드에는 Bean Validation에서 제공하는 @NotNull, @NotEmpty 등의 annotation을 사용할 수 있다.
+ *  하지만 사용자가 따로 정의한 필드에는 사용할 수 없기 때문에 원하는 Validation을 정의하고 사용해야 한다.
+ *
+ *  일반적으로 Enum 클래스를 정의하고 Validation할 때 사용할 때 유용하다.
+ *  Annotaion으로 사용할 Interface를 정의히고 valid 로직은 ConstraintValidator를 상속 받은 클래스에서 정의한다.
+ *
+ * @Reference
+ *  https://jsy1110.github.io/2022/enum-class-validation/ @Description JAVA에서 기본적으로 제공하는
+ *
+ */
 public class EnumValidator implements ConstraintValidator<Enum, String> {
 
     private List<String> enums;
