07fb7a545309f45839cc50892ede03d011bfdeba
justdoanything <pure_yong@naver.com>
2023-09-18 23:47:20 +0900

    내용 추가

---
+++ Lambda/Nodejs/src/model/dynamoDbModel.ts
@@ -0,0 +1,20 @@
+export interface AmazonConnectRequest {
+    Name: string;
+    Details: {
+        ContactData: {
+            Attributes: Record<string, string>;
+            Channel: string;
+            ContactId: string;
+            CustomerEndpoint: {
+                Address: string;
+                Type: string;
+            }
+            ...
+        }
+    }
+    Parameters?: Record<string, string> | null | undefined;
+}
+
+export interface AmazonConnectResponse {
+    ...
+}
---
+++ Lambda/Nodejs/src/service/dynamodb/dynamoDbService.ts
@@ -0,0 +1,13 @@
+const dynamoDbService = {
+    scanSample: async (
+        request: AmazonConnectRequest
+    ): Promse<AmazonConnectResponse> => {
+        try {
+            const response = await callDynamoDb(request);
+            return response.Count > 0 ? { ...response } : null;
+        } catch(exceptoin) {
+            logger.error("Excepton : " + exception);
+            throw new Error(exception);
+        }
+    }
+}
